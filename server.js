554970b2c3fbe785e6196dd70219710fd2abf900
lHATAl <hanabisinzou@gmail.com>
2021-11-16 22:47:00 +0900

    Inject dependency

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const http = require('http');
+const url = require('url');
+const { route } = require('./router');
 
 function start() {
 
     const onRequest = function (request, response) {
+        const pathname = url.parse(request.url).pathname;
+        console.log('request for ' + pathname + ' received.');
+
+        route(pathname);
+
         response.writeHead(200, {'Content-Type' : 'text/plain'});
         response.write('Hello nodejs');
         response.end();
